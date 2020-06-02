12b7c9fff63334056e772e1bf2a71f37d58044a8
davidreke <54542172+davidreke@users.noreply.github.com>
2020-06-02 16:37:02 -0400

    Updates

---
+++ precache-manifest.0483b6e8d09f277c5fac9094e3a1805d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13563e7409e77c847c59ee750e7dba86",
+    "url": "/basicTodoApp/index.html"
+  },
+  {
+    "revision": "0f104f79a71914823599",
+    "url": "/basicTodoApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d5147bdd77bca0333da1",
+    "url": "/basicTodoApp/static/js/2.172b186a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/basicTodoApp/static/js/2.172b186a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f104f79a71914823599",
+    "url": "/basicTodoApp/static/js/main.30c4a974.chunk.js"
+  },
+  {
+    "revision": "ebb4e9ecc0df88bbff5f",
+    "url": "/basicTodoApp/static/js/runtime-main.7798b9a6.js"
+  }
+]);
